8381d899191a45e438657c375b68cb6d25621e60
bkbn1 <100809480+bkbn1@users.noreply.github.com>
2024-11-05 11:06:32 -0600

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ from taskmanager import app
 # starting if this file is run directly using python run.py
 if __name__ == "__main__":
     app.run()
+from config import load_config
+app.config.from_object(load_config())
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
